bb8e7b6e303d7977070d19950eea131494225e36
Anabra <peter.d.podlovics@gmail.com>
2022-08-04 17:22:28 +0200

    add tests for util functions

--- test/gavle/util_test.clj
+++ test/gavle/util_test.clj
@@ -1,8 +1,24 @@
 (ns gavle.util-test
   (:require
    [clojure.test :refer :all]
+   [clojure.template :refer [do-template]]
    [gavle.util :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest update-in-if-exists
+  (do-template
+   [title m ks f expected]
+   (let [actual (sut/update-in-if-exists m ks f)]
+     (testing title
+       (is (= expected actual))))
+
+   "update existing nested key"
+   {:a {:b "x"}} [:a :b] keyword
+   {:a {:b :x}}
+
+   "update non-existent nested key"
+   {:a {:c "x"}} [:a :b] keyword
+   {:a {:c "x"}}
+
+   "update non-existent nested key (entire nested structure missing)"
+   {:a "x"} [:a :b] keyword
+   {:a "x"}))
